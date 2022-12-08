b0cad96726f47b1e84b76fc821ef0d0e416bb2da
unknown <marinotandria@gmail.com>
2022-12-08 18:23:51 +0300

    message

--- Header.php
+++ Header.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
-
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	
-	<link rel="stylesheet" href="style.css">
-    <!-- <link rel="stylesheet" href="animate.css"> -->
+	<link rel="stylesheet" href="Header/style.css">
+    <link rel="stylesheet" href="Header/animate.css">
 	</head>
 	<body>
 	<section class="ftco-section">
---
+++ Index.php
@@ -0,0 +1,4 @@
+
+<?php include 'Header.php';?>
+
+<?php include 'Footer.php';?>
